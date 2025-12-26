40072c37eb6a2ad9efe0d27408255d1764ed673f
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-27 00:01:50 +0100

    Add files via upload

---
+++ SWAP3-main/SWAP3/SWAP3/SWAP3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    int a, b, c, temp;
+    cout << "A= "; cin >> a;
+    cout << "B= "; cin >> b;
+    cout << "C= "; cin >> c;
+    cout << "A= " << a << ", B= " << b << ", C= " << c << endl;
+    temp = a;
+    a = c;
+    c = b;
+    b = temp;
+    cout << "A= " << a << ", B= " << b << ", C= " << c;
+}
